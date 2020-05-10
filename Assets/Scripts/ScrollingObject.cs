fc66f6cb130f2ffb4b7bf4b9e458cb95e5117298
yennyF <fung.yenny@gmail.com>
2020-05-10 19:41:23 -0400

    Fix: background gaps

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class ScrollingObject : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
+    // private Rigidbody2D rb2d;
     [SerializeField]
     private float scrollSpeed;
-    
-
-    private float groundWidth;
-
-    void Awake()
-    {
-        groundWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
 
     void Start()
     {
@@ -29,18 +21,6 @@ public class ScrollingObject : MonoBehaviour
         //     rb2d.velocity = Vector2.zero;
         // }
 
-        transform.position = new Vector2(transform.position.x + scrollSpeed  * Time.deltaTime , transform.position.y);
-
-        if (transform.position.x < -groundWidth)
-        {
-            RepositionBackground ();
-        }
-    }
-
-    private void RepositionBackground()
-    {
-        Vector2 groundOffSet = new Vector2(groundWidth * 2f, 0);
-
-        transform.position = (Vector2) transform.position + groundOffSet;
+        transform.position = new Vector2(transform.position.x + (scrollSpeed * Time.deltaTime), transform.position.y);
     }
 }
