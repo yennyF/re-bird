3f5bf50a0968fdeea0c5342b3396d67ff272a90a
yennyF <fung.yenny@gmail.com>
2020-05-10 18:40:47 -0400

    Parallax: added

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class ScrollingObject : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    [SerializeField]
+    private float scrollSpeed;
+    
+
+    private float groundWidth;
+
+    void Awake()
+    {
+        groundWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+        // rb2d = GetComponent<Rigidbody2D>();
+        // rb2d.velocity = new Vector2(scrollSpeed, 0);
     }
 
     void Update()
@@ -18,5 +28,19 @@ public class ScrollingObject : MonoBehaviour
         // {
         //     rb2d.velocity = Vector2.zero;
         // }
+
+        transform.position = new Vector2(transform.position.x + scrollSpeed  * Time.deltaTime , transform.position.y);
+
+        if (transform.position.x < -groundWidth)
+        {
+            RepositionBackground ();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffSet = new Vector2(groundWidth * 2f, 0);
+
+        transform.position = (Vector2) transform.position + groundOffSet;
     }
 }
