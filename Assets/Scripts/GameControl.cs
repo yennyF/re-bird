e0e3d8ab358fe4795370fdf5d8c6c87a9dd3d56c
yennyF <fung.yenny@gmail.com>
2020-05-10 00:24:17 -0400

    Background: scrolling

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public bool gameOver = false; 
+    public float scrollSpeed = -1.5f;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy (gameObject);
+    }
+}
---
+++ Assets/Scripts/RepeatingBacground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBacground : MonoBehaviour
+{
+    // private BoxCollider2D groundCollider;
+    private float groundHorizontalLength;
+    void Awake()
+    {
+        // groundCollider = GetComponent<BoxCollider2D> ();
+        // groundHorizontalLength = groundCollider.size.x;
+        groundHorizontalLength = 20.48f;
+        Debug.Log(groundHorizontalLength);
+    }
+
+    private void Update()
+    {
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            RepositionBackground ();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
+
+        transform.position = (Vector2) transform.position + groundOffSet;
+    }
+}
