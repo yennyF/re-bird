064988a20470f23ac083d52a154beacbeed3e19d
Cristhian Lunardi <clunardi@shokworks.io>
2020-05-10 15:58:56 -0400

    Main menu scene - Created (+ Start Button)

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    GameObject startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton = GameObject.Find("StartButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnHoverButton()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
