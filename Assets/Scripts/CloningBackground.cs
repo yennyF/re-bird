73581137571ee1381187c66f88c35ca67064802f
yennyF <fung.yenny@gmail.com>
2020-06-14 03:22:48 -0400

    Bacground Script: refactor

---
+++ Assets/Scripts/CloningBackground.cs
@@ -0,0 +1,27 @@
+﻿/*
+    author: nisa
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloningBackground : MonoBehaviour
+{
+    public GameObject[] layers;
+
+    void Start()
+    {
+        foreach(GameObject obj in layers)
+        {
+            GameObject clone = Instantiate(obj, obj.transform.parent) as GameObject;
+            float groundWidth = clone.GetComponent<SpriteRenderer>().bounds.size.x;
+
+            clone.transform.position = new Vector2(
+                obj.transform.position.x + groundWidth, 
+                obj.transform.position.y
+            );
+            clone.name = obj.name + 2;
+        }        
+    }
+}
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,51 @@
+﻿/*
+    author: nisa
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    private float groundWidth;
+    [SerializeField]
+    private float scrollSpeed;
+    
+    void Awake()
+    {
+        groundWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // void Start()
+    // {
+    //     rb2d = GetComponent<Rigidbody2D>();
+    //     rb2d.velocity = new Vector2(scrollSpeed, 0);
+    // }
+
+    private void Update()
+    {
+        // if(GameControl.instance.gameOver == true)
+        // {
+        //     rb2d.velocity = Vector2.zero;
+        // }
+
+        // transform.position = new Vector2(transform.position.x + (scrollSpeed * Time.deltaTime), transform.position.y);
+
+        transform.position = new Vector2(
+            transform.position.x + (scrollSpeed * Time.deltaTime), 
+            transform.position.y
+        );
+
+        if (transform.position.x < -groundWidth)
+        {
+            RepositionBackground ();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffSet = new Vector2(groundWidth * 2f, 0);
+        transform.position = (Vector2) transform.position + groundOffSet;
+    }
+}
