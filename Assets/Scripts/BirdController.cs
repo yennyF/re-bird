286f9746725b655e54273a9f4962e9912b9a6abf
Cristhian Lunardi <clunardi@shokworks.io>
2020-06-21 22:04:28 -0400

    Bird prefab

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -7,37 +7,21 @@ public class BirdController : MonoBehaviour
     public float velocity = 1;
     private Rigidbody2D rb;
     private Animator animator;
-    private SpriteRenderer spriteRenderer;
-    public Sprite flying, falling, dying;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("isFlying", false);
-
-        if (Input.GetMouseButtonDown(0)) // Left click
+        if (Input.GetMouseButtonDown(0)) // Left click | Jump
         {
-            // Jump
-            //spriteRenderer.sprite = flying;
-            animator.Play("BirdAnimation");
-
+            animator.SetTrigger("Fly");
             rb.velocity = Vector2.up * velocity;
-            //animator.SetBool("isFlying", true);
-            //Debug.Log(rb.velocity);
-        }
-
-        if (rb.velocity.y < 0)
-        {
-            
-            //spriteRenderer.sprite = falling;
         }
     }
 }
